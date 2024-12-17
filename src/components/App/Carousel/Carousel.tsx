7269b91d02a2775428144a708d3ce7926dd1c01a
Alexis Vignot <alexisvignot@hotmail.fr>
2024-12-17 21:29:36 +0100

    feat: carousel transition image

--- src/components/App/Carousel/Carousel.tsx
+++ src/components/App/Carousel/Carousel.tsx
@@ -1,76 +1,84 @@
-import { useEffect, useState, useCallback, useRef, MutableRefObject } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import { useEffect, useState, useCallback } from 'react';
 import { CarouselI } from '../../../@types/carousel';
 import './Carousel.scss'
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 function Carousel({ slides, arrows, indicators, interval }: CarouselI) {
   const [slideState, setSlideState] = useState(0);
-  const carouselRefs = useRef<(MutableRefObject<HTMLImageElement | null>)[]>([]);
+  const [transitionState, setTransitionState] = useState<'entering' | 'active' | 'inactive' | 'hidden'>('active');
+  const [nextSlide, setNextSlide] = useState<number>(0);
+
+  const setSlide = (index: number) => {
+    setNextSlide(index);
+    setTransitionState('inactive');
+
+    setTimeout(() => {
+      setSlideState(index);
+      setTransitionState('entering');
+      setTimeout(() => {
+        setTransitionState('active');
+      }, 200);
+    }, 200);
+  };
 
   const previousSlide = () => {
-    setSlideState(slideState === 0 ? slides.length - 1 : slideState - 1);
-  }
+    const previousIndex = slideState === 0 ? slides.length - 1 : slideState - 1;
+    setSlide(previousIndex);
+  };
 
-  const nextSlide = useCallback(() => {
-    setSlideState(slideState => slideState === slides.length - 1 ? 0 : slideState + 1);
-  }, [slides.length]);
+  const nextSlideHandler = useCallback(() => {
+    const nextIndex = slideState === slides.length - 1 ? 0 : slideState + 1;
+    setSlide(nextIndex);
+  }, [slideState, slides.length]);
 
   useEffect(() => {
     if (interval === 0) return;
-    const intervalSlide = setInterval(() => {
-      nextSlide();
-    }, interval * 1000);
-
+    const intervalSlide = setInterval(nextSlideHandler, interval * 1000);
     return () => clearInterval(intervalSlide);
-  }, [nextSlide, interval]);
+  }, [nextSlideHandler, interval]);
 
   return (
     <div className="carousel">
       {arrows && <IoIosArrowBack className="carousel_arrow carousel_arrow-left" onClick={previousSlide} />}
+      {slides.map((slide, index) => (
+        <div
+          key={index}
+          style={{ height: '100%' }}
+          className={`carousel_slide ${index === slideState
+            ? "carousel_slide-active"
+            : "carousel_slide-hidden"
+            }`}
+        >
+          <img src={slide.src} alt={slide.alt} className={`carousel_slide ${index === slideState
+            ? `carousel_slide-${transitionState}`
+            : index === nextSlide
+              ? 'carousel_slide-entering'
+              : 'carousel_slide-hidden'
+            }`} />
+          <img src={slides[nextSlide].src} alt={slides[nextSlide].alt} />
+          <div className="carousel_content">
+            {slide.title && <span className="carousel_content_title">{slide.title}</span>}
+            {slide.description && <span className="carousel_content_description">{slide.description}</span>}
+            {slide.button && <button className="carousel_content_button">{slide.button}</button>}
+          </div>
+        </div>
 
-      {
-        slides.map((slide, index) => {
-          return (
-            <div key={index} style={{ height: "100%" }}>
-              <CSSTransition nodeRef={carouselRefs.current[index]} in={slideState === index} timeout={300} classNames="fade" unmountOnExit key={index}>
-                <img ref={carouselRefs.current[index]} src={slide.src} alt={slide.alt} className={slideState === index ? "carousel_slide" : "carousel_slide carousel_slide-hidden"} />
-              </CSSTransition>
-
-              <div className={slideState === index ? "carousel_content" : "carousel_content-hidden"}>
-                {slide.title && <span className="carousel_content_title">{slide.title}</span>}
-                {slide.description && <span className="carousel_content_description">{slide.description}</span>}
-                {slide.button && <button className="carousel_content_button">{slide.button}</button>}
-              </div>
-
-            </div>
-
-          )
-        })
-      }
-
-      {arrows && <IoIosArrowForward className="carousel_arrow carousel_arrow-right" onClick={nextSlide} />}
-
-      {
-        indicators && (
-          <span className="carousel_indicators">
-            {
-              slides.map((_, index) => {
-                return (
-                  <button
-                    key={index}
-                    onClick={() => setSlideState(index)}
-                    className={slideState === index ? "carousel_indicators_indicator carousel_indicators_indicator-active" : "carousel_indicators_indicator carousel_indicators_indicator-inactive"}>
-                  </button>
-                )
-              })
-            }
-          </span>
-        )
-      }
-
+      ))}
+      {arrows && <IoIosArrowForward className="carousel_arrow carousel_arrow-right" onClick={nextSlideHandler} />}
+      {indicators && (
+        <span className="carousel_indicators">
+          {slides.map((_, index) => (
+            <button
+              key={index}
+              onClick={() => setSlide(index)}
+              className={`carousel_indicators_indicator ${nextSlide === index ? 'carousel_indicators_indicator-active' : 'carousel_indicators_indicator-inactive'
+                }`}
+            />
+          ))}
+        </span>
+      )}
     </div>
-  )
+  );
 }
 
 export default Carousel;
