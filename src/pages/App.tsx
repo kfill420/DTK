83e699993054c5891a441d68cf4ac961bffa9658
Alexis Vignot <alexisvignot@hotmail.fr>
2025-01-18 15:32:26 +0100

    fix: font downloaded

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,22 +1,23 @@
 import './App.scss'
-
-import HomePage from './HomePage/HomePage';
-import ProductPage from './ProductPage/ProductPage';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import ScrollToTop from '../components/App/ScrollToTop/ScrollToTop';
-import LoginPage from './LoginPage/LoginPage';
-import { useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../hooks/redux';
 import { actionCheckToken } from '../store/thunks/checkLogin';
 import { NonPrivateRoute, PrivateRoute } from '../components/App/PrivateRoute/PrivateRoute';
-import Order from './ProfilePage/Order/Order';
-import Params from './ProfilePage/Params/Params';
-import Infos from './ProfilePage/Infos/Infos';
 import SpinnerSquare from '../components/App/SpinnerSquare/SpinnerSquare';
-import CollectionPage from './CollectionPage/CollectionPage';
-import CartPage from "./CartPage/CartPage";
+import { lazy } from "react";
+
+const HomePage = lazy(() => import('./HomePage/HomePage.tsx'));
+const ProductPage = lazy(() => import('./ProductPage/ProductPage.tsx'));
+const LoginPage = lazy(() => import('./LoginPage/LoginPage.tsx'));
+const Order = lazy(() => import('./ProfilePage/Order/Order.tsx'));
+const Params = lazy(() => import('./ProfilePage/Params/Params.tsx'));
+const Infos = lazy(() => import('./ProfilePage/Infos/Infos.tsx'));
+const CollectionPage = lazy(() => import('./CollectionPage/CollectionPage.tsx'));
+const CartPage = lazy(() => import('./CartPage/CartPage.tsx'));
 
 function App() {
   const location = useLocation();
@@ -24,8 +25,8 @@ function App() {
   const noFooterPage = location.pathname === '/login' || location.pathname === '/params' || location.pathname === '/order' || location.pathname === '/profile';
   const isAuthentificated = useAppSelector((state) => state.account.isAuthentificated);
   const account = useAppSelector((state) => state.account.account);
-  const tokenIsLoading = useAppSelector((state) => state.account.tokenIsLoading);
-  const initialCheck = useAppSelector((state) => state.account.initialCheck);
+  // const tokenIsLoading = useAppSelector((state) => state.account.tokenIsLoading);
+  // const initialCheck = useAppSelector((state) => state.account.initialCheck);
   const modalIsOpen = useAppSelector((state) => state.ModalMenu.burgerModalIsOpen);
 
   useEffect(() => {
@@ -39,31 +40,33 @@ function App() {
       document.body.style.overflow = 'auto';
   }, [modalIsOpen]);
 
-  if (tokenIsLoading || initialCheck) {
-    return (
-      <div className="loader">
-        <SpinnerSquare isOpen={tokenIsLoading || initialCheck} />
-      </div>
-    )
-  }
+  // if (tokenIsLoading || initialCheck) {
+  //   return (
+  //     <div className="loader">
+  //       <SpinnerSquare isOpen={tokenIsLoading || initialCheck} />
+  //     </div>
+  //   )
+  // }
 
   return (
     <div className="app">
 
-      <SpinnerSquare isOpen={tokenIsLoading || initialCheck} />
+      {/* <SpinnerSquare isOpen={tokenIsLoading || initialCheck} /> */}
 
       <Header isAuthentificated={isAuthentificated} email={account.email} account_id={account.id} />
       <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/collection/:brand" element={<CollectionPage />} />
-        <Route path="/products/:id" element={<ProductPage />} />
-        <Route path="/login" element={<NonPrivateRoute isAuthenticated={isAuthentificated}><LoginPage /></NonPrivateRoute>}></Route>
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/params" element={<PrivateRoute isAuthenticated={isAuthentificated}><Params /></PrivateRoute>} />
-        <Route path="/order" element={<PrivateRoute isAuthenticated={isAuthentificated}><Order /></PrivateRoute>} />
-        <Route path="/profile" element={<PrivateRoute isAuthenticated={isAuthentificated}>{<Infos account={account} />}</PrivateRoute>} />
-      </Routes>
+      <Suspense fallback={<SpinnerSquare isOpen={true} />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/collection/:brand" element={<CollectionPage />} />
+          <Route path="/products/:id" element={<ProductPage />} />
+          <Route path="/login" element={<NonPrivateRoute isAuthenticated={isAuthentificated}><LoginPage /></NonPrivateRoute>}></Route>
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/params" element={<PrivateRoute isAuthenticated={isAuthentificated}><Params /></PrivateRoute>} />
+          <Route path="/order" element={<PrivateRoute isAuthenticated={isAuthentificated}><Order /></PrivateRoute>} />
+          <Route path="/profile" element={<PrivateRoute isAuthenticated={isAuthentificated}>{<Infos account={account} />}</PrivateRoute>} />
+        </Routes>
+      </Suspense>
       {!noFooterPage && <Footer />}
 
     </div>
