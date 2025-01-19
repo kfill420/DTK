c17bf7a20216d7f5a0ccbf821b4e8583c5a5559d
Alexis Vignot <alexisvignot@hotmail.fr>
2025-01-19 15:28:56 +0100

    feat: csrf in db

--- src/store/reducer/cart.ts
+++ src/store/reducer/cart.ts
@@ -51,7 +51,6 @@ const cartSlice = createSlice({
     });
     builder.addCase(actionCheckToken.fulfilled, (state, action) => {
       const ls = JSON.parse(localStorage.getItem('cartVisitor') || '[]');
-      console.log(action.payload.data.cart.productsCart);
 
       if (!ls || ls.length === 0)
         state.cartConnected = action.payload.data.cart.productsCart;
