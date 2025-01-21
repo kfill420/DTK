490800877826e24c112d08a878e84d10c6891fea
Alexis Vignot <alexisvignot@hotmail.fr>
2025-01-21 22:21:35 +0100

    feat: laoder in login modal

--- src/store/reducer/account.ts
+++ src/store/reducer/account.ts
@@ -23,6 +23,11 @@ import { actionAddToCart, actionDeleteOneFromCart } from "../thunks/checkCart";
 export const initialState = {
   id: null,
   isAuthentificated: false,
+  pending: {
+    checking: false,
+    login: false,
+    signup: false
+  },
   credentials: {
     email: '',
     passwordSignin: '',
@@ -112,12 +117,24 @@ const accountSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder.addCase(actionCheckConnexion.fulfilled, (state, action) => {
+      state.pending.checking = false;
       if (action.payload.exists === false)
         state.connection = 'signup';
       else
         state.connection = 'login';
     });
+    builder.addCase(actionCheckConnexion.pending, (state) => {
+      state.pending.checking = true;
+    });
+    builder.addCase(actionCheckConnexion.rejected, (state) => {
+      state.pending.checking = false;
+    });
     builder.addCase(actionCheckSignin.fulfilled, (state, action) => {
+      state.pending.login = false;
+      state.credentials.email = '';
+      state.credentials.passwordSignin = '';
+      state.credentials.password = '';
+      state.credentials.passwordConfirm = '';
       state.token = action.payload.data.token;
       state.isAuthentificated = true;
       state.account.id = action.payload.data.account.id;
@@ -152,14 +169,26 @@ const accountSlice = createSlice({
       state.credentials.password = '';
       state.credentials.passwordConfirm = '';
     });
+    builder.addCase(actionCheckSignin.pending, (state) => {
+      state.pending.login = true;
+    });
     builder.addCase(actionCheckSignin.rejected, (state, action) => {
       const payload = action.payload as { error: string };
+      state.pending.checking = false;
+      state.pending.login = false;
       state.credentials.errorSignup = payload.error;
       state.isAuthentificated = false;
     });
     builder.addCase(actionCheckSignup.fulfilled, (state) => {
+      state.pending.signup = false;
       state.connection = 'checking';
       state.credentials.errorSignup = null;
+    });
+    builder.addCase(actionCheckSignup.pending, (state) => {
+      state.pending.signup = true;
+    });
+    builder.addCase(actionCheckSignup.rejected, (state) => {
+      state.pending.signup = false;
     });
     builder.addCase(actionCheckToken.fulfilled, (state, action) => {
       state.isAuthentificated = action.payload.valid;
